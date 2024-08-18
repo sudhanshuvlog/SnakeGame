7e2fc57579392b442afde18e07ae02a7f5c74381
=sudhanshuvlog1 <pandeysudhanshuup@gmail.com>
2024-08-18 20:52:51 +0530

    update

--- test_app.py
+++ test_app.py
@@ -10,7 +10,7 @@ class FlaskAppTests(unittest.TestCase):
     def test_index_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'GFG Snake Game', response.data)
+        self.assertIn(b'GFG Game', response.data)
     
     def test_static_files(self):
         response = self.client.get('/static/game.js')
